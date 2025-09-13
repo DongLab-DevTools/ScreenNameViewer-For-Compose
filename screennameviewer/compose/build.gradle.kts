27cb82c811d92123c5fb452e84d45f00ab0ef389
김동현 <dongdong99@kakao.com>
2025-09-13 18:44:34 +0900

    feat : github packages 배포

--- screennameviewer/compose/build.gradle.kts
+++ screennameviewer/compose/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+
+    id("maven-publish")
 }
+
+group = "com.donglab.devtools"
+version = libs.versions.sdk.version.get()
 
 android {
     namespace = "com.donglab.screennameviewer.compose.lib"
@@ -36,6 +43,13 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.4"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -53,4 +67,38 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    repositories {
+        maven {
+            val props = Properties().apply {
+                load(rootProject.file("github.properties").inputStream())
+            }
+            url = uri(props.getProperty("url"))
+
+            credentials {
+                username = props.getProperty("github_username")
+                password = props.getProperty("github_token")
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                artifactId = "screennameviewer-compose"
+
+                // POM Metadata (Optional)
+                pom {
+                    name.set("ScreenNameViewer for Compose")
+                    description.set("Screen name viewer library for Jetpack Compose")
+                    url.set("https://github.com/DongLab-DevTools/ScreenNameViewer-For-Compose")
+                }
+            }
+        }
+    }
 }
