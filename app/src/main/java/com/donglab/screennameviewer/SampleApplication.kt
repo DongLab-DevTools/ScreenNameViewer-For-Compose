f1b92f3c40f34bcb7e505f2795cf8fd09dc0f433
dongx._.2 <dongdong99@kakao.com>
2025-09-15 11:20:10 +0900

    feat : 초기화 코드 변경

--- app/src/main/java/com/donglab/screennameviewer/SampleApplication.kt
+++ app/src/main/java/com/donglab/screennameviewer/SampleApplication.kt
@@ -1,22 +1,18 @@
 package com.donglab.screennameviewer
 
 import android.app.Application
-import com.donglab.screennameviewer.publicapi.ScreenNameViewer
-import com.donglab.screennameviewer.publicapi.config.ScreenNameOverlayConfig
-import com.donglab.screennameviewer.publicapi.setting.ScreenNameViewerSetting
+import com.donglab.screennameviewer.publicapi.initScreenNameViewer
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        ScreenNameViewer.initialize(
-            application = this,
-            settings = ScreenNameViewerSetting(
-                debugModeCondition = { true },
-                enabledCondition = { true }
-            ),
-            config = ScreenNameOverlayConfig.default()
-        )
+        initScreenNameViewer(this) {
+            settings {
+                debugMode { true }
+                enabled { true }
+            }
+        }
     }
 }
