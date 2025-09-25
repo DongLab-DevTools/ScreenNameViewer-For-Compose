611b12fc3a286c462a300daabab1cd788ce75f92
dongx._.2 <dongdong99@kakao.com>
2025-09-25 16:28:46 +0900

    feat : debug/release 환경 분리 샘플 추가

---
+++ app/src/debug/kotlin/com/donglab/screennameviewer/ScreenNameViewerInitializer.kt
@@ -0,0 +1,32 @@
+import android.app.Application
+import android.graphics.Color
+import android.view.Gravity
+import com.donglab.screennameviewer.publicapi.dsl.initScreenNameViewer
+
+object ScreenNameViewerInitializer {
+
+    fun init(application: Application) {
+        initScreenNameViewer(application) {
+            settings {
+                debugMode { true }
+                enabled { true }
+            }
+            config {
+                textStyle {
+                    size = 10f
+                    color = Color.BLUE
+                }
+                background {
+                    color = Color.argb(80, 255, 255, 255)
+                    padding = 20
+                }
+                position {
+                    topMargin = 52
+                    activity = Gravity.TOP or Gravity.START
+                    fragment = Gravity.TOP or Gravity.END
+                    composeRoute = Gravity.TOP or Gravity.END
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/donglab/screennameviewer/SampleApplication.kt
+++ app/src/main/java/com/donglab/screennameviewer/SampleApplication.kt
@@ -1,36 +1,12 @@
 package com.donglab.screennameviewer
 
 import android.app.Application
-import android.graphics.Color
-import android.view.Gravity
-import com.donglab.screennameviewer.publicapi.dsl.initScreenNameViewer
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        initScreenNameViewer(this) {
-            settings {
-                debugMode { true }
-                enabled { true }
-            }
-            config {
-                textStyle {
-                    size = 10f
-                    color = Color.BLUE
-                }
-                background {
-                    color = Color.argb(80, 255, 255, 255)
-                    padding = 20
-                }
-                position {
-                    topMargin = 52
-                    activity = Gravity.TOP or Gravity.START
-                    fragment = Gravity.TOP or Gravity.END
-                    composeRoute = Gravity.TOP or Gravity.END
-                }
-            }
-        }
+        ScreenNameViewerInitializer.init(this)
     }
 }
